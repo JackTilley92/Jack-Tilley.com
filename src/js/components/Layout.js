0ac0b3431b5496345bcc6dda8332ff8322f1b728
JackTilley92 <jack.tilley2@mail.dcu.ie>
2017-02-03 01:28:56 +0000

    White navbar

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,10 +4,12 @@ import AppBar from 'material-ui/AppBar';
 import AppBarExampleIcon from "./Bar";
 import React from 'react';
 import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
+import SwipeableViews from 'react-swipeable-views';
 import MenuItem from 'material-ui/MenuItem';
 import DropDownMenu from 'material-ui/DropDownMenu';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import {Tabs, Tab} from 'material-ui/Tabs';
 import ContactUs from './ContactUs';
@@ -28,61 +30,57 @@ const styles = {
     backgroundSize: 'cover',
     overflow: 'hidden'
   },
-  bg: {
-    backgroundColor: '#000000',
-    color: 'white'
-  },
   font:{
     fontSize: 40,
     paddingTop: 30,
     fontColor: 'white',
   },
-  white:{
+  tabStyle:{
     backgroundColor: '#FFFFFF',
-  }
+  },
 };
-
 export default class TabsExampleControlled extends React.Component {
 
   constructor(props) {
-      super(props);
-      this.state = {
-        slideIndex: 1,
-      };
-    }
+    super(props);
+    this.state = {
+      slideIndex: 0,
+    };
+  }
 
-    handleChange(value){
-         console.log("Tab is changing to ",value);
-          this.setState({
-            slideIndex: value,
-          });
-      };
+  handleChange = (value) => {
+    this.setState({
+      slideIndex: value,
+    });
+  };
 
   render() {
     return (
-      <MuiThemeProvider>
-        <div style={styles.bg}>
-          <figure>
-          </figure>
-          <Tabs value={this.state.slideIndex} onChange={this.handleChange}>
-            <Tab label="Jack" value={1}  style = {{backgroundColor: '#882601'}}>
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <div>
+          <Tabs
+              onChange={this.handleChange}
+              value={this.state.slideIndex}
+            >
+              <Tab label="Tab One" value={0} style = {styles.tabStyle} />
+              <Tab label="Tab Two" value={1} style = {styles.tabStyle} />
+              <Tab label="Tab Three" value={2} style = {styles.tabStyle} />
+            </Tabs>
+            <SwipeableViews
+              index={this.state.slideIndex}
+              onChangeIndex={this.handleChange}
+            >
               <div>
-                <h2 style={styles.headline}>Controllable Tab A</h2>
+                <h2 style={styles.headline}>Tabs with slide effect</h2>
                   <ProductsMenu />
               </div>
-            </Tab>
-            <Tab label="Projects" value={2} style = {{backgroundColor: '#882601'}}>
-              <div>
-                <h2 style={styles.headline}>Controllable Tab B</h2>
+              <div style={styles.slide}>
                   <Products />
               </div>
-            </Tab>
-            <Tab label="Blog" value={3} style = {{backgroundColor: '#882601'}}>
-              <div>
+              <div style={styles.slide}>
                 <ContactUs />
               </div>
-            </Tab>
-          </Tabs>
+            </SwipeableViews>
         </div>
       </MuiThemeProvider>
     );
